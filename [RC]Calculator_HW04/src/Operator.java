cb5298fbbd00b789c36d8ed48d77bbbe8ec4beeb
Jung yun su <haon4658@naver.com>
2017-04-04 15:18:19 +0900

    homework basic

---
+++ [RC]Calculator_HW04/src/Operator.java
@@ -0,0 +1,35 @@
+
+public enum Operator{
+	ADD("+"){
+		@Override
+		public double evaluate(double firstOperand,double secondOperand){ return firstOperand + secondOperand; }
+	},
+	SUB("-"){
+		@Override
+		public double evaluate(double firstOperand,double secondOperand){ return firstOperand - secondOperand; }
+	},
+	MUL("*"){
+		@Override
+		public double evaluate(double firstOperand,double secondOperand){ return firstOperand * secondOperand; }
+	},
+	DIV("/"){
+		@Override
+		public double evaluate(double firstOperand,double secondOperand){ return firstOperand / secondOperand; }
+	};
+	
+	private final String symbol;
+
+	Operator(String symbol){ this.symbol = symbol;}
+
+	public abstract double evaluate(double firstOperand, double secondOperand);
+
+	public static Operator findOperator(String symbol){
+		for(Operator op : values()){
+			if(op.symbol.equals(symbol)){
+				return op;
+			}
+		}
+		throw new RuntimeException("Invalid Operator");
+
+	}
+}
